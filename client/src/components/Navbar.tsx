dbd71edf6e75acee6a76e66de4bc0498a5b194d7
HarshitAgrawal2222 <agrawalharshit2003@gmail.com>
2026-05-28 23:25:08 +0530

    `Update client-side code: add new routes, components, and navigation links`

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -24,7 +24,7 @@ export default function Navbar() {
                 <div className="hidden md:flex items-center gap-8 transition duration-500">
                     <Link to='/' className="hover:text-pink-300 transition">Home</Link>
                     <Link to='/generate' className="hover:text-pink-300 transition">Generate</Link>
-                    <Link to='/my-generation' className="hover:text-pink-300 transition">Generations</Link>
+                    <Link to='/my-generation' className="hover:text-pink-300 transition">My Generations</Link>
                     <Link to='#' className="hover:text-pink-300 transition">Contact</Link>
                     
                 </div>
